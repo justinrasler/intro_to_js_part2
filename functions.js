3fc392cfc543df94cce199b4c9384a5a2547510b
justinrasler <85715506+justinrasler@users.noreply.github.com>
2025-06-25 13:26:48 -0400

    functions lab

---
+++ functions.js
@@ -0,0 +1,24 @@
+
+//arrow functions
+/*const helloWorld = () => {
+    console.log("Hello World")
+};
+
+//annonymous functions
+const helloWorld = function(){
+    console.log("Hello World")
+};
+
+//hoisted function
+function helloWorld(){
+    console.log("Hello World")
+}
+
+
+helloWorld() */
+
+const printGreeting = (name) => {
+return `Hello there ${name}`
+};
+
+console.log(printGreeting("Slimer"));
