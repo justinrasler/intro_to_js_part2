084579c691c7fa49d9ebfb90d3f3f90a6a8601d9
justinrasler <85715506+justinrasler@users.noreply.github.com>
2025-06-26 11:04:44 -0400

    struggling with functions, go back to this before you go to react

--- functions.js
+++ functions.js
@@ -22,3 +22,27 @@ return `Hello there ${name}`
 };
 
 console.log(printGreeting("Slimer"));
+
+//reverse word order
+
+const reverseWordOrder = (str) => {
+    const words = str.split(' ');
+    const reverseWordOrder = words.map(word => {
+        return word.split('').reverse().join('');
+    });
+    return reverseWordOrder.join(' ');
+}
+
+console.log(reverseWordOrder("Ishmael me Call"));
+
+
+function calculate(num1, num2, operation){
+    if(operation == "add"){
+        return num1 + num2
+    } else if(operation == "sub"){
+        return num1 - num2
+    }
+};
+
+console.log(calculate(4, 3, "sub"));
+console.log(calculate(4, 3, "add"));
