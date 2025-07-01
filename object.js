b4c80d31ad8a472307427b15882674ded6dde26d
justinrasler <85715506+justinrasler@users.noreply.github.com>
2025-06-30 21:43:39 -0400

    creating the robot exercise

--- object.js
+++ object.js
@@ -9,3 +9,24 @@
 // console.log(Justin);
 // console.log(Justin.name);
 // console.log(Justin["city"]);
+
+
+
+//create a Robot army
+
+//create a key
+const key = "weapon";
+
+//create a single robot
+const robot = {
+    name: "BleepBlop",
+    serial: 1,
+    isEvil: false,
+    ["Gas Tank"]: "Full",
+    [key]: "lazers"
+};
+
+console.log(robot);
+console.log(robot.name);
+console.log(robot.weapon);
+console.log(robot["Gas Tank"]);
