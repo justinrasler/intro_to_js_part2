d051f827d6f1786c3af559045386da9c4935608d
justinrasler <85715506+justinrasler@users.noreply.github.com>
2025-06-30 21:22:21 -0400

    objects part one lesson

---
+++ object.js
@@ -0,0 +1,11 @@
+//create an object
+// const Justin = {
+//     name: "Justin Rasler",
+//     age: 26,
+//     city: "suwanee",
+//     isAlive: true
+// };
+
+// console.log(Justin);
+// console.log(Justin.name);
+// console.log(Justin["city"]);
